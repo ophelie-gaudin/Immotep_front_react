ae24fe7250f17005e6b7cc7f6b5c5020b755baf1
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-03-13 16:23:18 +0100

    orange button atom workin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import OrangeButton from "./Components/Main/OrangeButton/index.jsx";
+
 function App() {
-  return <div className="text-red-300 font-black">Hello!!!</div>;
+  return (
+    <div className="text-oraange font-black">
+      Hello!!!
+      <OrangeButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Main/OrangeButton/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./OrangeButton.css";
+
+const OrangeButton = () => {
+  return (
+    <div>
+      <a className="orange-button p-4" href="/">
+        Mon bouton
+      </a>
+    </div>
+  );
+};
+
+export default OrangeButton;
