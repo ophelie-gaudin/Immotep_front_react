9b738d7a327911280505e3aecf2ca63c7f02b97f
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-03-13 17:39:25 +0100

    add a warning area atom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import OrangeButton from "./Components/Main/OrangeButton";
+import WarningArea from "./Components/Main/WarningArea";
 
 function App() {
-  return (
-    <div className="text-red-300 font-black">
-      Hello!!!
-      <OrangeButton />
-    </div>
-  );
+  return <div className="text-red-300 font-black">Hello!!!</div>;
 }
 
 export default App;
