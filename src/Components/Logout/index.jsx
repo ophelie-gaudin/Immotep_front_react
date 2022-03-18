1a32e63688fc884cbfef632e68a914e862b342b2
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-03-18 18:16:49 +0100

    fix hover logout button + add scrollbar website themed

--- src/Components/Logout/index.jsx
+++ src/Components/Logout/index.jsx
@@ -14,7 +14,12 @@ export default function LogOut() {
 
   return (
     <div>
-      <button onClick={() => logout()}>Déconnexion</button>
+      <button
+        className="block py-2 pr-4 pl-3 text-white rounded border-b border-light md:p-0 dark:text-white hover:bg-primary md:border-none  md:px-2 md:py:1  hover:font-bold"
+        onClick={() => logout()}
+      >
+        Déconnexion
+      </button>
     </div>
   );
 }
