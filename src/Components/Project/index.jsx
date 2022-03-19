ebccdcb872707cf8c5809a59b921a68b132f624b
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-19 20:15:07 +0100

    add a list in project#show view

--- src/Components/Project/index.jsx
+++ src/Components/Project/index.jsx
@@ -3,6 +3,7 @@ import Cookies from "js-cookie";
 import HousingCard from "../Main/HousingCard";
 import OrangeButton from "../Main/OrangeButton";
 import FormsCard from "../FormsCard";
+import { data } from "autoprefixer";
 
 export default function Project() {
   const [myHousings, setMyHousings] = useState([]);
@@ -35,15 +36,18 @@ export default function Project() {
         <br />
         <div className="flex w-full justify-end">
           <OrangeButton url={`/dashboard/${id_project}/housings/new`}>
-            Ajouter un Housing
+            J'ajoute un logement
           </OrangeButton>
         </div>
       </FormsCard>
     );
   } else {
     return (
-      <div>
-        <h1>Liste des housings de votre projet</h1>
+      <FormsCard
+        title="Biens repérés"
+        returnText="Mes projets"
+        returnUrl="/dashboard"
+      >
         {myHousings.map((data) => {
           return (
             <div key={data.id}>
@@ -51,11 +55,54 @@ export default function Project() {
             </div>
           );
         })}
+
+        <div class="bg-white dark:bg-gray-800  shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
+          <table class="w-full whitespace-nowrap">
+            <thead>
+              <tr
+                tabindex="0"
+                class="focus:outline-none h-16 w-full text-sm leading-none text-gray-800 dark:text-white "
+              >
+                <th class="font-normal text-left pl-4">Type de bien</th>
+                <th class="font-normal text-left pl-12">Prix</th>
+                <th class="font-normal text-left pl-12">Localisation</th>
+                <th class="font-normal text-left pl-20">Rentabilité</th>
+                {/* <th class="font-normal text-left pl-20">Deadline</th>
+                <th class="font-normal text-left pl-16">Members</th> */}
+              </tr>
+            </thead>
+            <tbody class="w-full">
+              {myHousings.map((data) => {
+                return (
+                  <tr
+                    key={data.id}
+                    tabindex="0"
+                    class="focus:outline-none h-20 text-sm leading-none text-gray-800 dark:text-white  bg-white dark:bg-gray-800  hover:bg-gray-100 dark:hover:bg-gray-900  border-b border-t border-gray-100 dark:border-gray-700 "
+                  >
+                    <td class="pl-4 cursor-pointer">
+                      <div class="flex items-center">
+                        {data.property_category}
+                      </div>
+                    </td>
+                    <td class="pl-12">{data.offer_price}</td>
+                    <td class="pl-12">{data.localization}</td>
+                    <td class="pl-20">{data.offer_profitability}</td>
+
+                    <td class="px-7 2xl:px-0 flex items-center">delete</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+
         <br />
-        <OrangeButton url={`/dashboard/${id_project}/housings/new`}>
-          Ajouter un Housing
-        </OrangeButton>
-      </div>
+        <div className="flex w-full justify-end">
+          <OrangeButton url={`/dashboard/${id_project}/housings/new`}>
+            J'ajoute un logement
+          </OrangeButton>
+        </div>
+      </FormsCard>
     );
   }
 }
