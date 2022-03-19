2b9806901741051473afb452f4722f4819fe36e2
Istarengwa <muller.lilian@outlook.fr>
2022-03-19 12:43:37 +0100

    add redirection in sucess delete housings

--- src/Components/HousingDelete/index.jsx
+++ src/Components/HousingDelete/index.jsx
@@ -10,26 +10,28 @@ export default function HousingDelete() {
   .substring(8, 10);
 
   const deletehousing = () => {
-  fetch(`https://immotep-api.herokuapp.com/projects/${id_project}/housings/${id_housings}`, {
+    fetch(`https://immotep-api.herokuapp.com/projects/${id_project}/housings/${id_housings}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: Cookies.get("token"),
       },
     })
-      .then((response) => response.json())
-      .then((response) => {
-        
-        window.location.href = `/dashboard/${id_project}`;
-      });
-    }
+    .then((response) => response.json())
+    .then((response) => {
+      
+      window.location.href = `/dashboard/${id_project}`;
+    });
+  }
 
-  
   return (
     <div>
       <button
         className="block py-2 pr-4 pl-3 text-white rounded border-b border-light md:p-0 dark:text-white hover:bg-primary md:border-none  md:px-2 md:py:1  hover:font-bold"
-        onClick={() => deletehousing()}
+        onClick={() => {
+          window.location.href = `/dashboard/${id_project}`
+          deletehousing()
+        }}
       >
         Supprimer le logement
       </button>
