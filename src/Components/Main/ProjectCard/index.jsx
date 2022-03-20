16d8a78b45f4f8ca30f580935d74dcb841279c9b
Istarengwa <muller.lilian@outlook.fr>
2022-03-20 05:24:13 +0100

    fix design ophé for projectcard

--- src/Components/Main/ProjectCard/index.jsx
+++ src/Components/Main/ProjectCard/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ProjectCard.css";
-// import { FaKey, FaMapPin } from "react-icons/fa";
-// import { FiArrowDownRight } from "react-icons/fi";
+import { FaMapPin } from "react-icons/fa";
+import { FiArrowDownRight } from "react-icons/fi";
 
 import { Link } from "react-router-dom";
 
@@ -14,12 +14,7 @@ const ProjectCard = (props) => {
     <Link to={`/dashboard/${data.id}`} className="project-card group"
       state={{data}}>
       <div className="project-card-text-scale">
-        <div className="project-card-title">{data.title}</div>
-        <div className="project-card-localization">{data.localization}</div>
-        <div className="project-card-comment">{data.comment}</div>
-        <div className="project-card-comment">
-              Voir plus d'informations
-        {/* <div className="project-card-localization flex items-center">
+        <div className="project-card-localization flex items-center">
           {" "}
           <span className="mr-4">
             <FaMapPin className="text-lg" />
@@ -30,7 +25,7 @@ const ProjectCard = (props) => {
         <div className="project-card-comment flex items-between w-full justify-end">
           <span>
             <FiArrowDownRight className="project-card-icon text-3xl font-bold group-hover:animate-bounce" />
-          </span> */}
+          </span>
         </div>
       </div>
     </Link>
