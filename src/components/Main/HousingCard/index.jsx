e23adcbda4bed252c65061210e55495b656c3de3
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-03-21 12:58:33 +0100

    fix redirections except return formCard

--- src/components/Main/HousingCard/index.jsx
+++ src/components/Main/HousingCard/index.jsx
@@ -1,34 +1,28 @@
-import React from "react";
-import { FaKey } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { FaKey } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const HousingCard = (props) => {
   const { data } = props;
-
-  const { housing_id, project_id } = useParams();
+  const housing_id = data.id;
+  const { project_id } = useParams();
 
   return (
-    <div className="project-card">
-      <div className="project-card-text-scale">
-        <div className="project-card-title">{data.ad_price}</div>
-        <div className="project-card-localization">{data.localization}</div>
-        <div className="project-card-comment">{data.notary_fees}</div>
+    <div className='project-card'>
+      <div className='project-card-text-scale'>
+        <div className='project-card-title'>{data.ad_price}</div>
+        <div className='project-card-localization'>{data.localization}</div>
+        <div className='project-card-comment'>{data.notary_fees}</div>
       </div>
       <span>
-        <FaKey className="project-card-icon rotate" />
+        <FaKey className='project-card-icon rotate' />
       </span>
-      <div className="project-card-comment">
-        <Link
-          to={`/dashboard/${project_id}/housing/${housing_id}/edit`}
-          state={{ data }}
-        >
+      <div className='project-card-comment'>
+        <Link to={`/dashboard/${project_id}/housing/${housing_id}/edit`}>
           Modifier le logement
         </Link>
-        <Link
-          to={`/dashboard/${project_id}/housing/${housing_id}`}
-          state={{ data }}
-        >
+        <Link to={`/dashboard/${project_id}/housing/${housing_id}`}>
           Voir le logement
         </Link>
       </div>
