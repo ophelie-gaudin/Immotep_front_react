6b9887b4be7a1718717b059b0650fd08aeaf55cd
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-21 16:13:24 +0100

    display of project

--- src/pages/Project/Project.read.jsx
+++ src/pages/Project/Project.read.jsx
@@ -4,9 +4,9 @@ import HousingCard from "../../components/Main/HousingCard";
 import OrangeButton from "../../components/Main/OrangeButton";
 import ProjectDelete from "../../components/ProjectDelete";
 import { useParams, Link } from "react-router-dom";
-// import FormsCard from "../FormsCard";
-// import { data } from "autoprefixer";
-import { FaEllipsisH, FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+import FormsCard from "../../components/FormsCard";
+
+import { FaEllipsisH } from "react-icons/fa";
 import { GoInfo } from "react-icons/go";
 
 export default function ProjectRead() {
@@ -35,101 +35,95 @@ export default function ProjectRead() {
   }, [housingArgument]);
 
   return (
-    <div className="mt-12 mb-8">
-      <div>
+    <FormsCard
+      title="Mon projet"
+      returnText="Mes projets"
+      returnUrl={`/dashboard`}
+    >
+      <div className="flex w-full justify-end mb-4">
+        <Link
+          to={`/dashboard/${project_id}/edit`}
+          className="text-primary hover:underline mr-4"
+        >
+          Modifier ce projet
+        </Link>
         <ProjectDelete data={project_id} />
-        <h1>Liste des housings de votre projet</h1>
-        {myHousings.map((data) => {
-          return (
-            <div key={data.id}>
-              <HousingCard key={data.id} data={data} />
-            </div>
-          );
-        })}
-
-        <div className="bg-white dark:bg-gray-800  shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
-          <table className="w-full whitespace-nowrap">
-            <thead>
-              <tr
-                tabIndex="0"
-                className="focus:outline-none h-16 w-full text-sm leading-none text-gray-800 dark:text-white "
-              >
-                <th className="font-normal text-center">Type de bien</th>
-                <th
-                  className="font-normal text-center "
-                  title="De base, nous affichons le prix de l'annonce. Dès que vous rentrez un prix d'offre, nous prenons ce dernier en compte."
-                >
-                  <div className="flex justify-center">
-                    Prix <GoInfo className="ml-2" />
-                  </div>
-                </th>
-                <th className="font-normal text-center">Localisation</th>
-                <th
-                  className="font-normal text-center"
-                  title="De base, notre calcul de rentabilité s'effectue avec le prix de l'annonce. Dès que vous rentrez un prix d'offre, nous prenons ce dernier en compte."
-                >
-                  <div className="flex  justify-center">
-                    Rentabilité <GoInfo className="ml-2" />
-                  </div>
-                </th>
-                <th className="font-normal text-center">
-                  Actions <br /> <small className="text-greey/70">[date]</small>
-                </th>
-                {/* <th className="font-normal text-left pl-16">Members</th> */}
-              </tr>
-            </thead>
-            <tbody className="w-full">
-              {myHousings.map((data) => {
-                return (
-                  <tr
-                    key={data.id}
-                    tabIndex="0"
-                    className="focus:outline-none h-20 text-sm leading-none text-gray-800 dark:text-white  bg-white dark:bg-gray-800  hover:bg-primary-light/20 dark:hover:bg-gray-900  border-b border-t border-gray-100 dark:border-gray-700 "
-                  >
-                    <td className="pl-12 flex items-center">
-                      {data.property_category}
-                    </td>
-                    <td className="pl-12">{data.offer_price}</td>
-                    <td className="pl-12">{data.localization}</td>
-                    <td className="pl-12">{data.offer_profitability}</td>
-
-                    <td className="h-full">
-                      <Link
-                        to={`/dashboard/${project_id}/housing/${data.id}`}
-                        className="flex flex-col justify-center items-center"
-                      >
-                        <span className="text-xl text-greey mb-2">
-                          <FaEllipsisH />
-                        </span>
-
-                        <small className="text-greey/70">
-                          {new Date(data.updated_at).toLocaleDateString(
-                            "en-US"
-                          )}
-                        </small>
-                      </Link>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-
-        <br />
-        <div className="flex w-full justify-end">
-          <Link
-            to={`/dashboard/${project_id}/edit`}
-            className="text-primary hover:underline mr-6"
-          >
-            Modifier ce projet
-          </Link>
-          <OrangeButton url={`/dashboard/${project_id}/housings/new`}>
-            J'ajoute un logement
-          </OrangeButton>
-        </div>
       </div>
-    </div>
+
+      <div className="bg-white dark:bg-gray-800  shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
+        <table className="w-full whitespace-nowrap">
+          <thead>
+            <tr
+              tabIndex="0"
+              className="focus:outline-none h-16 w-full text-sm leading-none text-gray-800 dark:text-white "
+            >
+              <th className="font-normal text-center">Type de bien</th>
+              <th
+                className="font-normal text-center "
+                title="De base, nous affichons le prix de l'annonce. Dès que vous rentrez un prix d'offre, nous prenons ce dernier en compte."
+              >
+                <div className="flex justify-center">
+                  Prix <GoInfo className="ml-2" />
+                </div>
+              </th>
+              <th className="font-normal text-center">Localisation</th>
+              <th
+                className="font-normal text-center"
+                title="De base, notre calcul de rentabilité s'effectue avec le prix de l'annonce. Dès que vous rentrez un prix d'offre, nous prenons ce dernier en compte."
+              >
+                <div className="flex  justify-center">
+                  Rentabilité <GoInfo className="ml-2" />
+                </div>
+              </th>
+              <th className="font-normal text-center">
+                Actions <br /> <small className="text-greey/70">[date]</small>
+              </th>
+              {/* <th className="font-normal text-left pl-16">Members</th> */}
+            </tr>
+          </thead>
+          <tbody className="w-full">
+            {myHousings.map((data) => {
+              return (
+                <tr
+                  key={data.id}
+                  tabIndex="0"
+                  className="focus:outline-none h-20 text-sm leading-none text-gray-800 dark:text-white  bg-white dark:bg-gray-800  hover:bg-primary-light/20 dark:hover:bg-gray-900  border-b border-t border-gray-100 dark:border-gray-700 "
+                >
+                  <td className="pl-12">{data.property_category}</td>
+                  <td className="pl-12">{data.offer_price}</td>
+                  <td className="pl-12">{data.localization}</td>
+                  <td className="pl-12">
+                    {data.offer_profitability || "Non calculée"}
+                  </td>
+
+                  <td className="h-full">
+                    <Link
+                      to={`/dashboard/${project_id}/housing/${data.id}`}
+                      className="flex flex-col justify-center items-center"
+                    >
+                      <span className="text-xl text-greey mb-2">
+                        <FaEllipsisH />
+                      </span>
+
+                      <small className="text-greey/70">
+                        {new Date(data.updated_at).toLocaleDateString("en-US")}
+                      </small>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      <br />
+      <div className="flex w-full justify-end">
+        <OrangeButton url={`/dashboard/${project_id}/housings/new`}>
+          J'ajoute un logement
+        </OrangeButton>
+      </div>
+    </FormsCard>
   );
 }
 
