3e8666a03c7a97a2dcd183ef179cce53218fe828
Istarengwa <muller.lilian@outlook.fr>
2022-03-22 10:44:28 +0100

    delete card housingcard

--- src/pages/Project/Project.read.jsx
+++ src/pages/Project/Project.read.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
-import HousingCard from "../../components/Main/HousingCard";
 import OrangeButton from "../../components/Main/OrangeButton";
 import ProjectDelete from "../../components/ProjectDelete";
 import { useParams, Link } from "react-router-dom";
