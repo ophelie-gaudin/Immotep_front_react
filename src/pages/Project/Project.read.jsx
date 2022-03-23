470886361d04b2a8f73bfd0374270f5c3647efa1
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-23 08:37:33 +0100

    add eye to list of housings

--- src/pages/Project/Project.read.jsx
+++ src/pages/Project/Project.read.jsx
@@ -5,7 +5,7 @@ import ProjectDelete from "../../components/Delete/Project.delete";
 import { useParams, Link } from "react-router-dom";
 import FormsCard from "../../components/FormsCard";
 
-import { FaEllipsisH } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
 import { GoInfo } from "react-icons/go";
 
 export default function ProjectRead() {
@@ -114,7 +114,8 @@ export default function ProjectRead() {
                 </div>
               </th>
               <th className="font-normal text-center">
-                Actions <br /> <small className="text-greey/70">[date]</small>
+                Voir les détails <br />{" "}
+                <small className="text-greey/70">[dernière modif]</small>
               </th>
               {/* <th className="font-normal text-left pl-16">Members</th> */}
             </tr>
@@ -139,10 +140,10 @@ export default function ProjectRead() {
                       to={`/dashboard/${project_id}/housing/${data.id}`}
                       className="flex flex-col justify-center items-center"
                     >
+                      {/* <span className="text-greey/80">Voir les détails</span> */}
                       <span className="text-xl text-greey mb-2">
-                        <FaEllipsisH />
+                        <FaRegEye />
                       </span>
-
                       <small className="text-greey/70">
                         {new Date(data.updated_at).toLocaleDateString("en-US")}
                       </small>
