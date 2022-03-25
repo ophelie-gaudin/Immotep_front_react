b7cab90f65392972f068b44801db7a3ce6672cff
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2022-03-25 22:50:24 +0100

    change logout width in navbar

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -18,7 +18,7 @@ export default function LogOut() {
   return (
     <div>
       <button
-        className="block py-2 pr-4 pl-3 text-white rounded border-b border-light md:p-0 dark:text-white hover:bg-primary md:border-none  md:px-2 md:py:1  hover:font-bold"
+        className="block py-2 pr-4 pl-3 text-white rounded border-b border-light md:p-0 dark:text-white hover:bg-primary md:border-none  md:px-2 md:py:1  hover:font-bold w-full text-left !px-0"
         onClick={() => logout()}
       >
         Déconnexion
